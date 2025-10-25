3c2c12b0960b5000c6786893d9b9bcb86b84ef5c
Sreevas-MK <sreevasmk@gmail.com>
2025-10-25 10:42:23 +0000

    Grafana test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`Hello from Node.js! Hostname: ${os.hostname()}\n`);
+  res.end(`Hello from Node.js app! Hostname: ${os.hostname()}\n`);
 });
 
 const PORT = 8080;
